9beaa64853b1a8212bfca8a616bf8f2cb30add47
Valerde <602533622@qq.com>
2022-07-13 01:56:59 +0800

    22.7.13commit

---
+++ ykn.sovava/剑指Offer26树的子结构/Solution.java
@@ -0,0 +1,38 @@
+package 剑指Offer26树的子结构;
+
+/**
+ * Description: 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * <p>
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ * <p>
+ * 例如:给定的树 A:
+ * <p>
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * 给定的树 B：
+ * <p>
+ *    4 
+ *   /
+ *  1
+ * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：A = [1,2,3], B = [3,1]
+ * 输出：false
+ * 示例 2：
+ * <p>
+ * 输入：A = [3,4,5,1,2], B = [4,1]
+ * 输出：true
+ *
+ * @author: ykn
+ * @date: 2022年07月13日 1:49
+ **/
+class Solution {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return false;
+    }
+}
---
+++ ykn.sovava/剑指Offer26树的子结构/TreeNode.java
@@ -0,0 +1,19 @@
+package 剑指Offer26树的子结构;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2022年07月13日 1:50
+ **/
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
