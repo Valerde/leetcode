8b11b8e9073fc6635080b43715741796be76bd5d
凯楠 杨 <602533622@qq.com>
2023-02-05 23:04:15 +0800

    23.2.5

---
+++ ykn.sovava/Cpp/1798. 你能构造出连续值的最大数目.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getMaximumConsecutive(vector<int>& coins) {
+	int res = 1;
+	sort(coins.begin(),coins.end());
+	for(auto&& i:coins){
+		if(i>res) break;
+		res+=i;
+	}
+	return res;
+    }
+};
