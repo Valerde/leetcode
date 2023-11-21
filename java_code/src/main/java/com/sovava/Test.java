78e9d7fc0a9698612ba28a35064faa46808894e6
root <602533622@qq.com>
2023-11-21 11:24:33 +0800

    https://leetcode.cn/problems/minimum-deletions-to-make-array-beautiful/description/

---
+++ java_code/src/main/java/com/sovava/Test.java
@@ -0,0 +1,13 @@
+package com.sovava;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2023年11月21日 10:57 AM
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
