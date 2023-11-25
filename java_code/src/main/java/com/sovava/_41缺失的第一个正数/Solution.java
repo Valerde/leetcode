7bc0d3b703aa27a95388b7fc61da1e5f68a42373
root <602533622@qq.com>
2023-11-25 10:30:19 +0800

    231125

---
+++ java_code/src/main/java/com/sovava/_41缺失的第一个正数/Solution.java
@@ -0,0 +1,32 @@
+package com.sovava._41缺失的第一个正数;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2023年11月25日 10:29 AM
+ **/
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = len + 1;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (Math.abs(nums[i]) <= len) {
+                nums[Math.abs(nums[i]) - 1] = -Math.abs(nums[Math.abs(nums[i]) - 1]);
+            }
+        }
+        int i = 0;
+        for (; i < len; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        return len + 1;
+    }
+}
