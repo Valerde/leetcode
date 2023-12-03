df5505201f715879b5556bc30f72b2380ec761c7
root <602533622@qq.com>
2023-12-03 14:52:14 +0900

    231202

---
+++ java_code/src/main/java/com/sovava/_1094拼车/Solution.java
@@ -0,0 +1,31 @@
+package com.sovava._1094拼车;
+
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2023年12月02日 9:52 AM
+ **/
+public class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+
+        int toMax = 0;
+        for (int[] trip : trips) {
+            toMax = Math.max(toMax, trip[2]);
+        }
+        int[] diff = new int[toMax + 1];
+        for (int[] trip : trips) {
+            diff[trip[1]] += trip[0];
+            diff[trip[2]] -= trip[0];
+        }
+        int res = 0;
+        for (int i = 0; i <= toMax; i++) {
+            res += diff[i];
+            if (res > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
