4fb3b7c53249f59818dfe926a1219b2941283adf
root <602533622@qq.com>
2023-12-04 11:38:25 +0900

    231204

---
+++ java_code/src/main/java/com/sovava/_1038从二叉搜索树到更大和树/Solution.java
@@ -0,0 +1,56 @@
+package com.sovava._1038从二叉搜索树到更大和树;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private int cnt = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        dfs(root.right);
+        cnt += root.val;
+        root.val = cnt;
+        dfs(root.left);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
