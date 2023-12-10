21a553415e7ba94d9813aabcabdbaef545c3816a
root <602533622@qq.com>
2023-12-10 12:57:06 +0900

    231210

---
+++ java_code/src/main/java/com/sovava/_70爬楼梯/Solution.java
@@ -0,0 +1,24 @@
+package com.sovava._70爬楼梯;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2023年12月10日 12:50 PM
+ **/
+public class Solution {
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int a = 1;
+        int b = 2;
+        int res = 0;
+        for(int i = 3;i<n;i++){
+            res = a+b;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+}
