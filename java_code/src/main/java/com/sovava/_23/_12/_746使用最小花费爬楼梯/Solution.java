58c0093e83f3624917c46796c307b38839442b1e
凯楠 杨 <602533622@qq.com>
2023-12-17 12:14:46 +0800

    231217

--- java_code/src/main/java/com/sovava/_23/_12/_746使用最小花费爬楼梯/Solution.java
+++ java_code/src/main/java/com/sovava/_23/_12/_746使用最小花费爬楼梯/Solution.java
@@ -9,14 +9,14 @@ package com.sovava._23._12._746使用最小花费爬楼梯;
 public class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int len = cost.length;
-        int[] dp = new int[len];
+        int[] dp = new int[len+1];
 
         dp[0] = dp[1] = 0;
 
-        for (int i = 2; i < len; i++) {
+        for (int i = 2; i <= len; i++) {
             dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
-        return dp[len-1];
+        return dp[len];
 
 
     }
