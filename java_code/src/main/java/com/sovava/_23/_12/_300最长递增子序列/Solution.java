4c11081dbfe8105e24e909e89990147467c0f99b
凯楠 杨 <602533622@qq.com>
2023-12-22 15:01:20 +0800

    add 300 another solution

--- java_code/src/main/java/com/sovava/_23/_12/_300最长递增子序列/Solution.java
+++ java_code/src/main/java/com/sovava/_23/_12/_300最长递增子序列/Solution.java
@@ -38,3 +38,28 @@ public class Solution {
     }
 
 }
+
+class Solution2 {
+    public int lengthOfLIS(int[] nums) {
+
+        int len = nums.length;
+
+        int[] dp = new int[len];
+        int res = -1;
+        for(int i = 0;i<len;i++){
+            dp[i] = 1;
+            for(int j = 0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+
+            }
+            res = Math.max(res,dp[i]);
+        }
+
+        return res;
+
+    }
+
+
+}
