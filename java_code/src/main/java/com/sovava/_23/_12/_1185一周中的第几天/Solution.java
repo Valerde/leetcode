5739f65169d187b91d23b5b502bddcf0ce9c7533
凯楠 杨 <602533622@qq.com>
2023-12-30 00:38:02 +0800

    231230

---
+++ java_code/src/main/java/com/sovava/_23/_12/_1185一周中的第几天/Solution.java
@@ -0,0 +1,7 @@
+package com.sovava._23._12._1185一周中的第几天;/** 
+ * Description: TODO
+ * @author: ykn
+ * @date: 2023年12月30日 0:35
+ **/
+ public class Solution {
+}
---
+++ java_code/src/main/java/com/sovava/_23/_12/_面试题1011峰与谷/Solution.java
@@ -0,0 +1,7 @@
+package com.sovava._23._12._面试题1011峰与谷;/** 
+ * Description: TODO
+ * @author: ykn
+ * @date: 2023年12月30日 0:16
+ **/
+ public class Solution {
+}
