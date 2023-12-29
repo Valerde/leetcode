a83d2698e03406fe732f11c0ce6c0007037a49ff
凯楠 杨 <602533622@qq.com>
2023-12-29 18:20:13 +0800

    231229

---
+++ java_code/src/main/java/com/sovava/_23/_12/_2706购买两块巧克力/Solution.java
@@ -0,0 +1,24 @@
+package com.sovava._23._12._2706购买两块巧克力;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2023年12月29日 18:19
+ **/
+public class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+        return min1 + min2 > money ? money : money - min1 - min2;
+    }
+}
