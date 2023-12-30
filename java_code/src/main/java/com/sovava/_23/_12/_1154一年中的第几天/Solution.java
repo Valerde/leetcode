49194c388ead2d88f73de7dae8aff75b19bfc811
凯楠 杨 <602533622@qq.com>
2023-12-31 00:29:01 +0800

    231231

---
+++ java_code/src/main/java/com/sovava/_23/_12/_1154一年中的第几天/Solution.java
@@ -0,0 +1,26 @@
+package com.sovava._23._12._1154一年中的第几天;
+
+/**
+ * Description: TODO
+ *
+ * @author: ykn
+ * @date: 2023年12月31日 0:28
+ **/
+public class Solution {
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            ++amount[1];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < month - 1; ++i) {
+            ans += amount[i];
+        }
+        return ans + day;
+    }
+}
